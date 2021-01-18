0813b985994e08aa5139f4d0652c277b825b6030
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-18 23:49:52 +0000

    Add route to /urls

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view enginer", "ejs");
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -19,8 +19,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
