1bbed1a4ef9fe0046e0573c0b52a069c835927b7
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-01-19 01:13:31 +0000

    Add random number generator

--- express_server.js
+++ express_server.js
@@ -6,10 +6,20 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+// RANDOM NUMBER FUNCTION
+const generateRandomString = function () {
+  return (+new Date()).toString(36).slice(-6);
+};
+
+// SERVER REQUESTS
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,11 +29,23 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// ALL URLS
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+// NEW URLS
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// FIND URL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
